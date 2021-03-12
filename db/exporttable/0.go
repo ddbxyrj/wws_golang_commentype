9b4c25329b4b31753d9d4291e43e026b7ffb5ac0
Administrator <admin@example.com>
2021-03-12 13:49:17 +0800

    Update db/exporttable/0.go

--- db/exporttable/0.go
+++ db/exporttable/0.go
@@ -1,12 +1,12 @@
 package exporttable
 
-type Export用户表1 struct {
+type Export用户表 struct {
 	ID        int
 	Uid       int `gorm:"Column:user_id"` //UNIQUE
 	Name      string
 	Show_name string
 }
 
-func (Export用户表1) TableName() string {
+func (Export用户表) TableName() string {
 	return "surha_user_true"
 }

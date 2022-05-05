7a032e5dadf9aa052243e20f83a20f8782facdd0
gitaccount <gitaccount@example.com>
2022-05-05 09:22:10 +0000

    1

---
+++ wwsTranType/userLevel.go
@@ -0,0 +1,11 @@
+package wwsTranType
+
+type UserLevelMethod interface {
+	FindTopInstitutionNum(*[]int) []int
+}
+
+type UserLevel struct {
+	Org    *[]int
+	Posit  *[]int
+	Method *UserLevelMethod
+}
